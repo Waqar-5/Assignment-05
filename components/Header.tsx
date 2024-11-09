5be38f05b6b2e5e6625c050fd6dfa2455db3c594
waqar-5 <wa5134810@gmail.com>
2024-11-09 08:31:30 +0000

    Figma Design in Next.Js

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-[130px] bg-[#A29875] pb-24" >
+      {/* <span>
+      <input type="text" placeholder="Search for Gold jewellery, diamonds jewellery and more... " className="w-[769.89px] h-[65.24px] rounded-xl mb-auto ml-[431.7px]  border-2 text-[#FFFFFF]	 "  />
+      </span> */}
+      <h1 className="w-[400px] text-[75px] pt-20px pl-[36px]  text-[#FFFFFF] " >
+        MANZZARI
+        </h1> 
+      
+      
+      
+
+    </div>
+  );
+}
